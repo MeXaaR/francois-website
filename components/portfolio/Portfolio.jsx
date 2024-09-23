934a436cd2f2a73232ba492f6bb5db52c8992784
Francois AUBEUT <francois.aubeut-gervaise@carrier.com>
2024-09-23 13:12:11 +0300

    change footer

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -128,7 +128,7 @@ export default function Portfolio() {
 
             <div className="footer-copyright text-center pt-25 pb-25 bg-light-white-2 border-redius-20">
               <span>
-                © {new Date().getFullYear()} All Rights Reserved by ib-themes.
+                © {new Date().getFullYear()} All Rights Reserved by François Aubeut.
               </span>
             </div>
           </div>
