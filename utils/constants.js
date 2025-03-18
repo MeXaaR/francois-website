bd735c75566a90e44e20f264d3b7f19d455c5537
Francois AUBEUT <contact@mexar.fr>
2025-03-18 10:17:06 +0100

    add black back

--- utils/constants.js
+++ utils/constants.js
@@ -7,5 +7,8 @@ export const LINE_WIDTH = 2
 export const MIN_TURN_FRAMES = 60
 export const MAX_TURN_FRAMES = 180
 
+export const BG_COLOR_LIGHT = 'white'
+export const BG_COLOR_DARK = '#000000'
+
 export const LINE_COLOR_LIGHT = '#000000'
 export const LINE_COLOR_DARK = 'darkorange'
